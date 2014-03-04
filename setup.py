d8502079cef7f562c057385440428003a592dca5
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2014-03-04 17:48:29 +0100

    0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-project-info',
-    version='0.1.0',
+    version='0.2.0',
     description="A small plugin which takes a configuration (like Bower or npm) and it provides data via context processors.",
     keywords='django, project-info',
     author='Lukas Rychtecky',
