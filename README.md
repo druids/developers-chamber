7765f91b6b4e3b65b3ec2c5d932cbcf13a609697
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2014-03-04 12:15:46 +0100

    Added setup.py and a context processor

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 django-project-info
 ===================
 
-A small plugin which takes a configuration (like Bower or npm) and data are displayed via context processors.
+A small plugin which takes a configuration (like Bower or npm) and it provides data via context processors.
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-project-info',
+    version='0.1.0',
+    description="A small plugin which takes a configuration (like Bower or npm) and it provides data via context processors.",
+    keywords='django, project-info',
+    author='Lukas Rychtecky',
+    author_email='lukas.rychtecky@gmail.com',
+    url='https://github.com/LukasRychtecky/django-project-info',
+    license='MIT',
+    package_dir={'project_info': 'project_info'},
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        'django>=1.6',
+    ],
+    zip_safe=False
+)
