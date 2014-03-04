c9c8b88b9fa5d521bbb4b10db0e659b0d9276e2e
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2014-03-04 18:04:29 +0100

    Wrapped release into function closes #3

--- project_info/release.py
+++ project_info/release.py
@@ -1,101 +1,101 @@
 from django.conf import settings
 import json
-import sys
 import os.path
 import git
 import re
 
-config = {
-    'files': ['pip.json'],
-    'next_branch': 'next',
-    'origin_repo': 'origin',
-    'project_dir': settings.PROJECT_DIR
-}
-custom = settings.RELEASE_CONFIG or {}
-config.update(custom)
 
-origin = config['origin_repo']
-next_branch = config['next_branch']
-pwd = config['project_dir']
+RELEASE_TYPES = ('major', 'minor', 'patch')
 
-release_type = 'patch'
-if len(sys.argv) > 1:
-    release_type = sys.argv[1]
 
-RELEASE_TYPES = ['major', 'minor', 'patch']
+def release(release_type='patch'):
 
-if not release_type in RELEASE_TYPES:
-    info = release_type, ', '.join(RELEASE_TYPES)
-    print 'An invalid release type given: %s \nExpected: %s' % info
-    sys.exit(1)
+    config = {
+        'files': ['pip.json'],
+        'next_branch': 'next',
+        'origin_repo': 'origin',
+        'project_dir': settings.PROJECT_DIR
+    }
+    custom = settings.RELEASE_CONFIG or {}
+    config.update(custom)
 
-if len(config['files']) is 0:
-    print 'Given no files to release a version'
-    sys.exit(2)
+    origin = config['origin_repo']
+    next_branch = config['next_branch']
+    pwd = config['project_dir']
 
-# GitPython does not support merge --no-ff or what?
-git_cmd = git.cmd.Git(pwd)
+    if not release_type in RELEASE_TYPES:
+        info = release_type, ', '.join(RELEASE_TYPES)
+        print 'An invalid release type given: %s \nExpected: %s' % info
+        return 1
 
-repo = git.Repo(pwd)
-g = repo.git
+    if len(config['files']) is 0:
+        print 'Given no files to release a version'
+        return 2
 
-g.checkout(next_branch)
+    # GitPython does not support merge --no-ff or what?
+    git_cmd = git.cmd.Git(pwd)
 
-current_version = '0.1.0'
-any_config = os.path.join(pwd, config['files'][0])
-if any_config and os.path.isfile(any_config):
-    handler = open(any_config)
-    json_data = json.load(handler)
-    current_version = json_data['version'] or current_version
-    handler.close()
+    repo = git.Repo(pwd)
+    g = repo.git
 
-tokens = current_version.split('.')
-print( current_version)
-if not len(tokens) is 3:
-    print 'An invalid version format given: %' % current_version
-    sys.exit(3)
+    g.checkout(next_branch)
 
-version = map(int, tokens)
-
-if release_type == 'patch':
-    version[2] += 1
-elif release_type == 'minor':
-    version[1] += 1
-    version[2] = 0
-else:
-    version[0] += 1
-    version[1] = 0
-    version[2] = 0
-
-str_version = '.'.join(str(item) for item in version)
-g.pull(origin, next_branch)
-
-release_branch = "release-" + str_version
-g.checkout(next_branch, b=release_branch)
-
-regexp = re.compile('"version": "[0-9][0-9]*\.[0-9][0-9]*(\.)?([0-9][0-9]*)?"')
-substitution = '"version": "%s"' % str_version
-for f in config['files']:
-    try:
-        handler = open(os.path.join(pwd, f), 'r+')
-        replaced = regexp.sub(substitution, handler.read())
-        handler.seek(0)
-        handler.write(replaced)
+    current_version = '0.1.0'
+    any_config = os.path.join(pwd, config['files'][0])
+    if any_config and os.path.isfile(any_config):
+        handler = open(any_config)
+        json_data = json.load(handler)
+        current_version = json_data['version'] or current_version
         handler.close()
-    except IOError, e:
-        print 'An error occurred: %s' % e
-        exit(4)
 
-g.add(config['files'])
-g.commit(m=str_version)
-g.tag(str_version)
+    tokens = current_version.split('.')
+    if not len(tokens) is 3:
+        print 'An invalid version format given: %' % current_version
+        return 3
 
-g.push(origin, release_branch)
+    version = map(int, tokens)
 
-no_ff_commit = "Merge branch 'release-%s'" % str_version
-for branch in ('master', next_branch):
-    g.checkout(branch)
-    git_cmd.execute(('git', 'merge', '--no-ff', '-m', no_ff_commit, release_branch))
-    g.push(origin, branch)
+    if release_type == 'patch':
+        version[2] += 1
+    elif release_type == 'minor':
+        version[1] += 1
+        version[2] = 0
+    else:
+        version[0] += 1
+        version[1] = 0
+        version[2] = 0
 
-git_cmd.execute(('git', 'push', origin, '--tags'))
+    str_version = '.'.join(str(item) for item in version)
+    g.pull(origin, next_branch)
+
+    release_branch = "release-" + str_version
+    g.checkout(next_branch, b=release_branch)
+
+    regexp = re.compile('"version": "[0-9][0-9]*\.[0-9][0-9]*(\.)?([0-9][0-9]*)?"')
+    substitution = '"version": "%s"' % str_version
+    for f in config['files']:
+        try:
+            handler = open(os.path.join(pwd, f), 'r+')
+            replaced = regexp.sub(substitution, handler.read())
+            handler.seek(0)
+            handler.write(replaced)
+            handler.close()
+        except IOError, e:
+            print 'An error occurred: %s' % e
+            return 4
+
+    g.add(config['files'])
+    g.commit(m=str_version)
+    g.tag(str_version)
+
+    g.push(origin, release_branch)
+
+    no_ff_commit = "Merge branch 'release-%s'" % str_version
+    for branch in ('master', next_branch):
+        g.checkout(branch)
+        git_cmd.execute(('git', 'merge', '--no-ff', '-m', no_ff_commit, release_branch))
+        g.push(origin, branch)
+
+    git_cmd.execute(('git', 'push', origin, '--tags'))
+
+    return 0
