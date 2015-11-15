5eee33586de0f67288761fc228e62bd09536eea0
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2015-11-15 12:19:23 +0100

    Added a support for Python 3.* and more info messages fixes #7 #8

--- project_info/release.py
+++ project_info/release.py
@@ -1,21 +1,72 @@
+from __future__ import print_function
+
 import json
 import os.path
 import git
 import re
+import sys
+import operator
 
 
 RELEASE_TYPES = ('major', 'minor', 'patch')
 
+VERSION_RE = re.compile('"version": "[0-9][0-9]*\.[0-9][0-9]*(\.)?([0-9][0-9]*)?"')
+
+VERSION_DELTA = {
+    'major': (1, 0, 0),
+    'minor': (0, 1, 0),
+    'patch': (0, 0, 1),
+}
+
+
+def make_collection(string_or_tuple):
+    return (string_or_tuple,) if isinstance(string_or_tuple, str) else string_or_tuple
+
+
+def error(msg):
+    [print(message, file=sys.stderr) for message in make_collection(msg)]
+
+
+def info(msg):
+    [print(message, file=sys.stdout) for message in make_collection(msg)]
+
+
+def write_version(project_dir, f, substitution, str_version):
+    try:
+        handler = open(os.path.join(project_dir, f), 'r+')
+        replaced = VERSION_RE.sub(substitution, handler.read())
+        handler.seek(0)
+        handler.write(replaced)
+        handler.close()
+        return ('Version bumped to {} (in {})'.format(str_version, f), None)
+    except IOError as e:
+        return (None, 'An error occurred: {}'.format(e))
+
+
+def compose_version(release_type, current_version):
+    delta = VERSION_DELTA[release_type] if release_type in VERSION_DELTA else VERSION_DELTA['patch']
+    return map(operator.add, current_version, delta)
+
+
+def merge_branch(git_cmd, g, branch, no_ff_commit, release_branch, origin):
+    g.checkout(branch)
+    git_cmd.execute(('git', 'merge', '--no-ff', '-m', no_ff_commit, release_branch))
+    g.push(origin, branch)
+    return ('{} to {}'.format(no_ff_commit, branch), 'Pushed {} to origin'.format(branch))
+
+
+def compact(seq):
+    return tuple(item for item in seq if item is not None)
+
 
 def release(project_dir, release_type='patch', files=['pip.json'], next_branch='next', origin='origin'):
 
-    if not release_type in RELEASE_TYPES:
-        info = release_type, ', '.join(RELEASE_TYPES)
-        print 'An invalid release type given: %s \nExpected: %s' % info
+    if release_type not in RELEASE_TYPES:
+        error('An invalid release type given: {} \nExpected: {}'.format(release_type, ', '.join(RELEASE_TYPES)))
         return 1
 
     if len('files') is 0:
-        print 'Given no files to release a version'
+        error('Given no files to release a version')
         return 2
 
     # GitPython does not support merge --no-ff or what?
@@ -36,51 +87,38 @@ def release(project_dir, release_type='patch', files=['pip.json'], next_branch='
 
     tokens = current_version.split('.')
     if not len(tokens) is 3:
-        print 'An invalid version format given: %' % current_version
+        error('An invalid version format given: {}'.format(current_version))
         return 3
 
-    version = map(int, tokens)
-
-    if release_type == 'patch':
-        version[2] += 1
-    elif release_type == 'minor':
-        version[1] += 1
-        version[2] = 0
-    else:
-        version[0] += 1
-        version[1] = 0
-        version[2] = 0
-
+    version = compose_version(release_type, map(int, tokens))
     str_version = '.'.join(str(item) for item in version)
     g.pull(origin, next_branch)
 
     release_branch = "release-" + str_version
     g.checkout(next_branch, b=release_branch)
 
-    regexp = re.compile('"version": "[0-9][0-9]*\.[0-9][0-9]*(\.)?([0-9][0-9]*)?"')
-    substitution = '"version": "%s"' % str_version
-    for f in files:
-        try:
-            handler = open(os.path.join(project_dir, f), 'r+')
-            replaced = regexp.sub(substitution, handler.read())
-            handler.seek(0)
-            handler.write(replaced)
-            handler.close()
-        except IOError, e:
-            print 'An error occurred: %s' % e
-            return 4
+    substitution = '"version": "{}"'.format(str_version)
+    infos, errors = tuple(compact(msgs)
+                          for msgs in tuple(zip(*tuple(write_version(project_dir, f, substitution, str_version)
+                                                       for f in files))))
+    if len(errors):
+        [error(err) for err in errors]
+        return 4
+
+    [info(msg) for msg in infos]
 
     g.add(files)
     g.commit(m=str_version)
+    info('Committed as "{}"'.format(str_version))
     g.tag(str_version)
+    info('Tagged as "{}"'.format(str_version))
 
     g.push(origin, release_branch)
 
-    no_ff_commit = "Merge branch 'release-%s'" % str_version
-    for branch in ('master', next_branch):
-        g.checkout(branch)
-        git_cmd.execute(('git', 'merge', '--no-ff', '-m', no_ff_commit, release_branch))
-        g.push(origin, branch)
+    no_ff_commit = "Merge branch 'release-{}'".format(str_version)
+    for msg in (merge_branch(git_cmd, g, branch, no_ff_commit, release_branch, origin)
+                for branch in ('master', next_branch)):
+        info(msg)
 
     git_cmd.execute(('git', 'push', origin, '--tags'))
 
