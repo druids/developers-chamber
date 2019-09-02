d1c60e0d66953a72b5b92ecd48df3f115846267d
Radim Sückr <contact@radimsuckr.cz>
2019-09-02 08:15:21 +0200

    One-off task running in AWS ECS

--- developers_chamber/scripts/ecs.py
+++ developers_chamber/scripts/ecs.py
@@ -3,6 +3,7 @@ import os
 import click
 
 from developers_chamber.ecs_utils import deploy_new_task_definition as deploy_new_task_func
+from developers_chamber.ecs_utils import run_task as run_task_func
 from developers_chamber.ecs_utils import stop_service as stop_service_func
 from developers_chamber.scripts import cli
 
@@ -16,20 +17,31 @@ def ecs():
 
 
 @ecs.command()
-@click.option('--cluster', help='ecs cluster name', type=str, required=True)
-@click.option('--service', help='ecs service names', type=str, required=True)
-@click.option('--task-definition', help='ecs task definition name', type=str, required=True)
+@click.option('--cluster', help='ECS cluster name', type=str, required=True)
+@click.option('--service', help='ECS service names', type=str, required=True)
+@click.option('--task-definition', help='ECS task definition name', type=str, required=True)
 @click.option('--image', help='docker image tag to use', type=str, required=True)
-@click.option('--region', help='aws region', type=str, default=default_region, required=True)
+@click.option('--region', help='AWS region', type=str, default=default_region, required=True)
 def deploy_new_task_definition(cluster, service, task_definition, image, region):
     """Deploy new task definition in AWS ECS."""
     click.echo(deploy_new_task_func(cluster, service, task_definition, image, region))
 
 
 @ecs.command()
-@click.option('--cluster', help='ecs cluster name', type=str, required=True)
-@click.option('--service', help='ecs service name', type=str, required=True)
-@click.option('--region', help='aws region', type=str, default=default_region, required=True)
+@click.option('--cluster', help='ECS cluster name', type=str, required=True)
+@click.option('--service', help='ECS service name', type=str, required=True)
+@click.option('--region', help='AWS region', type=str, default=default_region, required=True)
 def stop_service(cluster, service, region):
     """Stop an AWS ECS service by updating its size and setting it to 0."""
     click.echo(stop_service_func(cluster, service, region))
+
+
+@ecs.command()
+@click.option('--cluster', help='ECS cluster name', type=str, required=True)
+@click.option('--task-definition', help='ECS task definition name', type=str, required=True)
+@click.option('--command', help='command to run', type=str, required=True)
+@click.option('--name', help='ECS task name', type=str, required=True)
+@click.option('--region', help='AWS region', type=str, default=default_region, required=True)
+def run_task(cluster, task_definition, command, name, region):
+    """Run a single task in AWS ECS."""
+    click.echo(run_task_func(cluster, task_definition, command, name, region))
