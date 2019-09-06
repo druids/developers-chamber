40470ea1688be32ee0dfd3d451dc2eb03d2fffd9
Lubos Matl <lubos.matl@mallpay.czm>
2019-09-06 14:44:40 +0200

    0.0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='developers-chamber',
-    version='0.0.8',
+    version='0.0.9',
     description='A small plugin which help with development, deployment, git',
     keywords='django, skripts, easy live, git, bitbucket, Jira',
     author='Druids team',
