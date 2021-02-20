bc5f9e435b692523b4ce1bbf74075b530f3039b4
David Třebický <davidtrebicky@gmail.com>
2021-02-20 14:07:51 +0100

    chore(upgrade-boto3): pin boto3 version to < 2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'click==7.0',
         'requests==2.23.0',
         'python-dotenv==0.14.0',
-        'boto3==1.9.230',
+        'boto3<2',
         'python-hosts==0.4.6',
         'isort==4.3.21',
         'coloredlogs==10.0',
