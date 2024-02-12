4dfd210ef10e2f92f23e85bddff8b6b939d55e28
Radim Sückr <radim.suckr@skippay.cz>
2024-02-12 13:02:42 +0100

    chore: bump to v1.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='skip-developers-chamber',
-    version='1.1.0',
+    version='1.2.0',
     description='A small plugin which help with development, deployment, git',
     keywords='django, skripts, easy live, git, bitbucket, Jira',
     author='Druids team',
