7f60e5a1c6ad659fd4b225816df16cb8983d350f
Luboš Mátl <matllubos@gmail.com>
2024-06-18 18:58:51 +0200

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='developers-chamber',
-    version='0.1.0',
+    version='0.1.2',
     description='A small plugin which help with development, deployment, git',
     keywords='django, skripts, easy live, git, bitbucket, Jira',
     author='Druids team',
