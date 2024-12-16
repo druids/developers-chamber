c1c59c0254b879eac4221290eb876e1d763b3e0d
Peter Kubov <peter.kubov@skippay.cz>
2024-12-16 18:26:48 +0100

    chore: release 1.5.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='skip-developers-chamber',
-    version='1.5.3',
+    version='1.5.4',
     description='A small plugin which help with development, deployment, git',
     keywords='django, skripts, easy live, git, bitbucket, Jira',
     author='Druids team',
